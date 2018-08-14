29b15bcbf7d03e1a190ee4cc857143c96bd17909
Krzysztof Musiał <krzysztof.musial@mwplkramaclt033.logthemin.com>
2018-08-14 14:15:48 +0200

    krj first commit

---
+++ src/App/src/components/Home.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
+
+class Home extends Component {
+  render() {
+    return (
+      <div>Home sweet home!</div>
+    );
+  }
+}
+
+export default Home;
