7b05628a582397d09f51e919f0dc993da83f0df4
k-r-j <krzysztof.musial@makingwaves.com>
2018-09-20 16:58:37 +0200

    CurrentCharts + TotalNumbers components styles

--- src/App/src/screens/home/Home.js
+++ src/App/src/screens/home/Home.js
@@ -1,15 +1,17 @@
 import React, { Component } from 'react';
 import VideoHeader from './VideoHeader.js';
-import BfmProgress from './BfmProgress.js';
-import Slider from './Slider.js';
+import TotalNumbers from './TotalNumbers.js';
+import CurrentCharts from './CurrentCharts.js';
+import CharitySlider from './CharitySlider.js';
 
 class Home extends Component {
   render() {
     return (
       <div className="Home">
         <VideoHeader/>
-        <BfmProgress/>
-        <Slider/>
+        <TotalNumbers/>
+        <CurrentCharts/>
+        <CharitySlider/>
       </div>
     );
   }
