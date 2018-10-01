6cd74db4a95024e456f51ce52ca67a54ab1d79c9
Paweł Maga <pawel.p.maga@gmail.com>
2018-10-01 11:08:15 +0200

    Results record should be formatted as JSON

--- src/Functions/Persistance/SqlScripts/Script0008- Create Strava.AthleteMonthlyResults table.sql
+++ src/Functions/Persistance/SqlScripts/Script0008- Create Strava.AthleteMonthlyResults table.sql
@@ -1,1 +1,5 @@
-﻿CREATE TABLE dbo.[Strava.AthleteMonthlyResults] (Id int primary key identity, Date datetime2 NOT NULL, AthleteId int UNIQUE NOT NULL, Results nvarchar(max) NOT NULL, FOREIGN KEY (AthleteId) REFERENCES dbo.[Strava.Athletes](AthleteId) )
+﻿CREATE TABLE dbo.[Strava.AthleteMonthlyResults] (Id int primary key identity, Date datetime2 NOT NULL, AthleteId int UNIQUE NOT NULL, Results nvarchar(4000) NOT NULL, FOREIGN KEY (AthleteId) REFERENCES dbo.[Strava.Athletes](AthleteId) )
+GO
+ALTER TABLE dbo.[Strava.AthleteMonthlyResults]
+    ADD CONSTRAINT [Results record should be formatted as JSON]
+                   CHECK (ISJSON(Results)=1)
