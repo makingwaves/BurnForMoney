93c62e8fba7e062065b57e150bbae995eefad87f
Paweł Maga <pawel.p.maga@gmail.com>
2018-10-02 12:03:23 +0200

    added better payload for support functions.

--- src/Functions/Support/ActivitiesOperations.cs
+++ src/Functions/Support/ActivitiesOperations.cs
@@ -1,9 +1,8 @@
 ﻿using System;
-using System.Net;
-using System.Net.Http;
 using System.Threading.Tasks;
 using BurnForMoney.Functions.Helpers;
 using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.Azure.WebJobs;
 using Microsoft.Azure.WebJobs.Extensions.Http;
 using Microsoft.Extensions.Logging;
@@ -13,18 +12,19 @@ namespace BurnForMoney.Functions.Support
     public static class ActivitiesOperations
     {
         [FunctionName(FunctionsNames.Support_Strava_Activities_Collect)]
-        public static async Task<HttpResponseMessage> Support_Strava_CollectActivities([HttpTrigger(AuthorizationLevel.Admin, "get", Route = "support/strava/activities/collect")]HttpRequest req, ILogger log, 
+        public static async Task<IActionResult> Support_Strava_CollectActivities([HttpTrigger(AuthorizationLevel.Admin, "get", Route = "support/strava/activities/collect")]HttpRequest req, ILogger log, 
             [OrchestrationClient]DurableOrchestrationClient starter)
         {
             log.LogInformation($"{FunctionsNames.Support_Strava_Activities_Collect} function processed a request.");
             string optimize = req.Query["optimize"];
             var instanceId = await starter.StartNewAsync(FunctionsNames.O_CollectStravaActivities, optimize);
 
-            return starter.CreateCheckStatusResponse(new HttpRequestMessage(), instanceId);
+            var payload = starter.CreateHttpManagementPayload(instanceId);
+            return new OkObjectResult(payload);
         }
 
         [FunctionName(FunctionsNames.Support_Strava_Activities_CollectMonthlyStatistics)]
-        public static async Task<HttpResponseMessage> Support_Strava_Activities_MohlyStatisticsCollect([HttpTrigger(AuthorizationLevel.Admin, "get", Route = "support/strava/activities/collectmonthlystatistics")]HttpRequest req, ILogger log,
+        public static async Task<IActionResult> Support_Strava_Activities_MohlyStatisticsCollect([HttpTrigger(AuthorizationLevel.Admin, "get", Route = "support/strava/activities/collectmonthlystatistics")]HttpRequest req, ILogger log,
             [OrchestrationClient]DurableOrchestrationClient starter)
         {
             log.LogInformation($"{FunctionsNames.Support_Strava_Activities_CollectMonthlyStatistics} function processed a request.");
@@ -32,20 +32,23 @@ namespace BurnForMoney.Functions.Support
             string month = req.Query["month"];
             if (string.IsNullOrWhiteSpace(month))
             {
-                log.LogWarning("Function invoked with incorrect parameters. [month] is null or empty.");
-                return new HttpResponseMessage(HttpStatusCode.BadRequest);
+                var errorMessage = "Function invoked with incorrect parameters. [month] is null or empty.";
+                log.LogWarning(errorMessage);
+                return new BadRequestObjectResult(errorMessage);
             }
 
             string year = req.Query["year"];
             if (string.IsNullOrWhiteSpace(year))
             {
-                log.LogWarning("Function invoked with incorrect parameters. [year] is null or empty.");
-                return new HttpResponseMessage(HttpStatusCode.BadRequest);
+                var errorMessage = "Function invoked with incorrect parameters. [year] is null or empty.";
+                log.LogWarning(errorMessage);
+                return new BadRequestObjectResult(errorMessage);
             }
 
             var instanceId = await starter.StartNewAsync(FunctionsNames.O_CalculateMonthlyAthleteResults, new DateTime(int.Parse(year), int.Parse(month), 1));
 
-            return starter.CreateCheckStatusResponse(new HttpRequestMessage(), instanceId);
+            var payload = starter.CreateHttpManagementPayload(instanceId);
+            return new OkObjectResult(payload);
         }
     }
 }
