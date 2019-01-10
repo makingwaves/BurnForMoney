05241bb44d8c42a5a569c7f1a916b1b99229ad32
k-r-j <krzysztof.musial@makingwaves.com>
2019-01-10 16:35:29 +0100

    Ranking functionality extrend to all categories

--- src/App/src/screens/dashboard/Dashboard/Dashboard.js
+++ src/App/src/screens/dashboard/Dashboard/Dashboard.js
@@ -10,13 +10,19 @@ const Dashboard = (props) =>{
       <DashboardHeader header="Dashboard" />
       <div className="Dashboard-content DashboardGrid">
         <div className="DashboardGridItem DashboardGridItem-summary">
-          Summry
+          <p>Summry</p>
         </div>
         <div className="DashboardGridItem DashboardGridItem-results">
           Results
         </div>
         <div className="DashboardGridItem DashboardGridItem-ranking">
-          <Ranking ranking={props.ranking} categories={props.categories} rankCategory={props.rankCategory}/>
+          <Ranking
+            ranking={props.ranking}
+            rankingCategory={props.rankingCategory}
+            setRankinkCategory={props.setRankinkCategory}
+            categories={props.categories}
+            rankCategory={props.rankCategory}
+          />
         </div>
         <div className="DashboardGridItem DashboardGridItem-contribution">
           Activities Contribution
