ca83881fe2e48a20600b508b2c472681eab4e52a
Paweł Maga <pawel.p.maga@gmail.com>
2019-03-13 20:29:13 +0100

    api for top dashboard

---
+++ src/BurnForMoney.Functions.InternalApi/Functions/Dashboard/DashboardFunc.cs
@@ -0,0 +1,38 @@
+using System.Threading.Tasks;
+using BurnForMoney.Functions.InternalApi.Configuration;
+using BurnForMoney.Functions.Shared.Functions.Extensions;
+using BurnForMoney.Infrastructure.Persistence.Repositories;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Extensions.Http;
+using Microsoft.Extensions.Logging;
+
+namespace BurnForMoney.Functions.InternalApi.Functions.Dashboard
+{
+    public class DashboardFunc
+    {
+        [FunctionName(FunctionsNames.GetDashboardTop)]
+        public static async Task<IActionResult> GetDashboardTop([HttpTrigger(AuthorizationLevel.Anonymous, "get", Route = "dashboardtop")] HttpRequest req,  
+            ILogger log, [Configuration] ConfigurationRoot configuration)
+        {
+            int? month = null;
+            int? year = null;
+            var monthParameter = req.Query["month"];
+            if (!string.IsNullOrWhiteSpace(monthParameter))
+            {
+                month = int.Parse(monthParameter);
+            }
+            var yearParameter = req.Query["year"];
+            if (!string.IsNullOrWhiteSpace(yearParameter))
+            {
+                year = int.Parse(yearParameter);
+            }
+
+            var repository = new DashboardReadRepository(configuration.ConnectionStrings.SqlDbConnectionString);
+            var dashboardTop = await repository.GetDashboardTopAsync(month, year);
+       
+            return new OkObjectResult(dashboardTop);
+        }
+    }
+}
---
+++ src/BurnForMoney.Infrastructure/Persistence/Repositories/Dto/DashboardTop.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace BurnForMoney.Infrastructure.Persistence.Repositories.Dto
+{
+    public class DashboardTop
+    {
+        public double Distance { get; set; }
+        public double Time { get; set; }
+        public int Points { get; set; }
+    }
+}
